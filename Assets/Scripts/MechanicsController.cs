134da23e60fd61d7c00d92569986276d71fcae67
AlerHugu3s <axiasiming@gmail.com>
2021-07-10 17:51:52 +0800

    修改上传

--- Assets/Scripts/MechanicsController.cs
+++ Assets/Scripts/MechanicsController.cs
@@ -10,10 +10,22 @@ public class MechanicsController : MonoBehaviour
         LineMode,
         GlobalMode
     }
+    public enum TriggerMode
+    {
+        Infinite,
+        Temp
+    }
     [Range(0, 200)] public float WindForce;
     public Vector3 windVec = Vector3.zero;
 
     public WindMode windMode = WindMode.LineMode;
+
+    public TriggerMode triggerMode = TriggerMode.Infinite;
+
+    private bool isPause = false;
+
+    [SerializeField,Range(0,10)]
+    private float activeTime, pauseTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +33,30 @@ public class MechanicsController : MonoBehaviour
     }
     void OnTriggerStay2D(Collider2D coll)
     {
-        switch (coll.tag)
+        if (!isPause)
         {
-            case "Dandelion":
-                GameEventDispatcher.GetInstance()
-                    .DispatchEvent(new BaseGameEvent(CoreController.GameEventType.DANDELION_GET_WIND, null, this));
-                break;
+            if (triggerMode == TriggerMode.Temp)
+                StartCoroutine(pauseEnumerator());
+            switch (coll.tag)
+            {
+                case "Dandelion":
+                    GameEventDispatcher.GetInstance()
+                        .DispatchEvent(new BaseGameEvent(CoreController.GameEventType.DANDELION_GET_WIND, null, this));
+                    break;
+            }
         }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+
+    }
+
+    IEnumerator pauseEnumerator()
+    {
+        yield return new WaitForSecondsRealtime(activeTime);
+        isPause = true;
+        yield return new WaitForSecondsRealtime(pauseTime);
+        isPause = false;
     }
 }
